df241053d8af5f1416bb208392b2e394931c88c2
Just Yaviz (Yahyobek Tohirjonov) <justyaviz@gmail.com>
2026-09-15 11:55:43 +0500

    Wire CRM v0.9.5 students API

--- eduka2/server.js
+++ eduka2/server.js
@@ -91,6 +91,7 @@ const centerV082AcademicSettings = require('./routes/center-v082-academic-settin
 const centerV083Branches = require('./routes/center-v083-branches');
 const centerV093Leads = require('./routes/center-v093-leads');
 const centerV094Groups = require('./routes/center-v094-groups');
+const centerV095Students = require('./routes/center-v095-students');
 const tenantRoutes = require('./routes/tenant-api');
 app.use('/api', apiRoutes);
 app.use('/api/app', centerRoutes);
@@ -103,6 +104,7 @@ app.use('/api/app', centerV082AcademicSettings);
 app.use('/api/app', centerV083Branches);
 app.use('/api/app', centerV093Leads);
 app.use('/api/app', centerV094Groups);
+app.use('/api/app', centerV095Students);
 app.use('/api/tenant', tenantRoutes);
 
 // Static assets.
